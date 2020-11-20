d3fe8e13a67fb764864f3346189b8a01f3fdedd8
Hofsté Menno <hofstem1@piet.org.aalto.fi>
2020-11-21 00:42:06 +0200

    move to tensorboard logging

--- train.py
+++ train.py
@@ -4,64 +4,47 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import gym
 import numpy as np
+from torch.utils import tensorboard
 
 import wimblepong
 from agent import Agent
 
+writer = tensorboard.SummaryWriter()
 # Load the environment
 # env = gym.make("WimblepongVisualSimpleAI-v0")
-env = gym.make("WimblepongSimpleAI-v0")
-player = Agent(env)
+env = gym.make("CartPole-v0")
+player = Agent(env, writer=writer)
 # player.load_model('results/model_16.mdl')
 
 episodes = 1000
-env.set_names(player.get_name())
+# env.set_names(player.get_name())
 
-rew_hist = []
-rew_running_avg1 = -10
-rew_running_avg2 = -10
-rew_running_avg_hist1 = []
-rew_running_avg_hist2 = []
-ep = 0
+global_iter = 0
+episode_n = 0
 while True:
-    rew_sum = 0
+    episode_n += 1
+
     done = False
-    ob = env.reset()
-
-    # Run the episode itself
-    length = 0
+    reward_sum = 0
+    next_state = env.reset()
     while not done:
-        action, action_prob = player.get_action(ob)
+        state = next_state
+        action = player.get_action(state, False)
+        next_state, reward, done, info = env.step(action)
 
-        ob, rew, done, info = env.step(action)
+        player.store_outcome(action, state, next_state, reward, done)
 
-        player.store_outcome(ob, action, action_prob, rew)
+        reward_sum += reward
+        global_iter += 1
 
-        rew_sum += rew
-        length += 1
-        # env.render()
-
-    rew_hist.append(rew_sum)
-    rew_running_avg1 = 0.99 * rew_running_avg1 + 0.01 * rew_sum
-    rew_running_avg_hist1.append(rew_running_avg1)
-    rew_running_avg2 = 0.999 * rew_running_avg2 + 0.001 * rew_sum
-    rew_running_avg_hist2.append(rew_running_avg2)
-    ep += 1
-
-    if ep % 10 == 0:
-        print(f'Episode: {ep:>5}, length: {length:>5}, running1: {rew_running_avg1:>7}, running2: {rew_running_avg2:>7}')
+    writer.add_scalar('reward/episode_reward', reward_sum, global_iter)
 
     # Update the model based on the last batch_size episodes
-    batch_size = 200
-    if ep % batch_size == batch_size - 1:
+    batch_size = 5
+    if episode_n % batch_size == batch_size - 1:
         player.update_policy()
 
-    save_rate = 1000
-    if ep % save_rate == 0:
-        plt.figure()
-        plt.plot(rew_running_avg_hist1, label='Rolling average (100)')
-        plt.plot(rew_running_avg_hist2, label='Rolling average (1000)')
-        plt.legend()
-        plt.title("Reward history")
-        plt.savefig(f'results/fig_{ep // save_rate}.png')
-        player.save_model(f'results/model_{ep // save_rate}.mdl')
+    save_rate = 10000
+    if episode_n % save_rate == 0:
+        quit()
+        player.save_model(f'results/model_{episode_n // save_rate}.mdl')
