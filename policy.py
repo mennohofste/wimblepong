093fcccfa3224c22504f474789321d3393ca5b69
Hofsté Menno <hofstem1@piet.org.aalto.fi>
2020-11-25 20:42:09 +0200

    move to conv layers and fix discounted rewards

--- policy.py
+++ policy.py
@@ -4,51 +4,65 @@ from torch import nn
 from torch.nn import functional as F
 from torch.distributions import Categorical
 
+EPS_CLIP = 0.1
+BETA = 0.01
 
 class Policy(nn.Module):
     def __init__(self):
-        super().__init__()
-        self.gamma = 0.99
-        self.eps_clip = 0.2
-
-        self.fc1 = nn.Linear(200 * 200, 256)
-        self.fc2 = nn.Linear(256, 3)
+        super(Policy, self).__init__()
+        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.size = 9 * 9 * 64
+        
+        self.fc1 = nn.Linear(self.size, 512)
+        self.fc2 = nn.Linear(512, 2)
 
     def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(-1,self.size)
         x = F.relu(self.fc1(x))
         return F.softmax(self.fc2(x), dim=-1)
 
-    def pre_process(self, x, prev_x):
-        if prev_x is None:
-            return self.state_to_tensor(x)
-        return self.state_to_tensor(x) - self.state_to_tensor(prev_x)
-        # return torch.cat([self.state_to_tensor(x), self.state_to_tensor(prev_x)])
+    def pre_process(self, x, prev_x, prev2_x, prev3_x):
+        x = self.state_to_tensor(x)
+        prev_x = self.state_to_tensor(prev_x)
+        prev2_x = self.state_to_tensor(prev2_x)
+        prev3_x = self.state_to_tensor(prev3_x)
+        output = torch.stack([x, prev_x, prev2_x, prev3_x,])
+        return output.unsqueeze(0)
 
     def state_to_tensor(self, x):
-        x = torch.from_numpy(x)
+        if x is None:
+            return torch.zeros((100, 100))
         # Normalise color space
-        x = torch.sum(x, dim=-1) / (3 * 255)
-        x = torch.flatten(x)
-        return x
+        x = x[::2, ::2, 1]
+        x[x == 1] = 0
+        x[x != 0] = 1
+        return torch.from_numpy(x).float()
 
-    def get_loss(self, d_obs, action, action_prob_old, advantage):
-        action_prob = torch.sum(self.forward(d_obs) * F.one_hot(action, num_classes=3), dim=1)
+    def get_loss(self, states, action, action_prob_old, advantage):
+        action_prob = self.forward(states)
+        action_prob = torch.sum(action_prob * F.one_hot(action, 2), dim=1)
+
+        entropy = action_prob * action_prob_old.log() + \
+            (1 - action_prob) * action_prob_old.log()
         r = action_prob / action_prob_old
         loss1 = r * advantage
-        loss2 = torch.clamp(r, 1 - self.eps_clip, 1 + self.eps_clip) * advantage
-        loss = -torch.min(loss1, loss2)
-        loss = torch.mean(loss)
-        return loss
+        loss2 = torch.clip(r, 1 - EPS_CLIP, 1 + EPS_CLIP) * advantage
+        loss = torch.min(loss1, loss2)
+        loss = torch.mean(loss - BETA * entropy)
+        return -loss
 
-    def get_action(self, state, evaluation=True):
+    def get_action(self, state):
         '''
         Interface function which returns an action of type int given an observation
         '''
         with torch.no_grad():
-            action_prob = self.forward(state)
+            action_prob = self.forward(state).squeeze()
+            dist = Categorical(action_prob)
+            action = dist.sample().item()
 
-        if evaluation:
-            return torch.argmax(action_prob).item()
-        else:
-            action = Categorical(action_prob).sample().item()
-            return action, action_prob[action]
+        return action, action_prob[action]
--- train.py
+++ train.py
@@ -9,25 +9,28 @@ import wimblepong
 from policy import Policy
 
 # Learning parameters
-EPS_PER_EPOCH = 100
+GAMMA = 0.99
+EPS_PER_ITERATION = 10
+EPOCHS = 5
 MAX_TIMESTEPS = 500
-LEARNING_RATE = 1e-3
+LEARNING_RATE = 1e-4
 
 def collect_data():
     global global_n
     
-    d_obs_history, action_history, action_prob_history, reward_history = [], [], [], []
-    for _ in range(EPS_PER_EPOCH):
-        obs, prev_obs = env.reset(), None
-        for t in range(MAX_TIMESTEPS):
-            d_obs = policy.pre_process(obs, prev_obs)
-            with torch.no_grad():
-                action, action_prob = policy.get_action(d_obs, False)
+    state_history, action_history, action_prob_history, reward_history = [], [], [], []
+    for _ in range(EPS_PER_ITERATION):
+        obs, prev_obs, prev2_obs, prev3_obs = env.reset(), None, None, None
 
-            prev_obs = obs
+        for t in range(MAX_TIMESTEPS):
+            state = policy.pre_process(obs, prev_obs, prev2_obs, prev3_obs)
+            prev_obs, prev2_obs, prev3_obs = obs, prev_obs, prev2_obs
+
+            with torch.no_grad():
+                action, action_prob = policy.get_action(state)
             obs, reward, done, _ = env.step(action)
 
-            d_obs_history.append(d_obs)
+            state_history.append(state)
             action_history.append(action)
             action_prob_history.append(action_prob)
             reward_history.append(reward)
@@ -39,69 +42,69 @@ def collect_data():
         writer.add_scalar('reward/episode_length', t, global_n)
         global_n += 1
     
-    return [d_obs_history, action_history, action_prob_history, reward_history]
+    return [state_history, action_history, action_prob_history, reward_history]
 
 def compute_advantages(reward_history):
-    # compute advantage
     R = 0
     discounted_rewards = torch.zeros(len(reward_history))
-
     for i, r in enumerate(reward_history[::-1]):
-        if r != 0:  # scored/lost a point in pong, so reset reward sum
+        # scored a point so reset the cumulation
+        if r != 0:
             R = 0
-        R = r + policy.gamma * R
-        discounted_rewards[i] = R
+        R = r + GAMMA * R
+        discounted_rewards[-i] = R
 
     discounted_rewards -= discounted_rewards.mean()
-    discounted_rewards /= discounted_rewards.std()
+    discounted_rewards /= discounted_rewards.std() + 1.0e-10
     return discounted_rewards
 
 def update_policy(data):
-    d_obs_history = data.pop(0)
+    state_history = data.pop(0)
     action_history = data.pop(0)
     action_prob_history = data.pop(0)
     advantage_history = data.pop(0)
 
     # update policy
-    for _ in range(10):
-        n_batch = len(action_history) // 4
+    for _ in range(EPOCHS):
+        n_batch = len(action_history)
         idxs = random.sample(range(len(action_history)), n_batch)
-        d_obs_batch = torch.stack([d_obs_history[idx] for idx in idxs])
+        state_batch = torch.cat([state_history[idx] for idx in idxs], 0)
         action_batch = torch.LongTensor([action_history[idx] for idx in idxs])
         action_prob_batch = torch.FloatTensor([action_prob_history[idx] for idx in idxs])
         advantage_batch = torch.FloatTensor([advantage_history[idx] for idx in idxs])
 
         opt.zero_grad()
-        loss = policy.get_loss(d_obs_batch, action_batch, action_prob_batch, advantage_batch)
+        loss = policy.get_loss(state_batch, action_batch, action_prob_batch, advantage_batch)
+        writer.add_scalar('loss/loss', -loss, global_n)
         loss.backward()
         opt.step()
 
-def train_iteration(i):
-    data = collect_data()
-    reward_history = data.pop(3)
-    advantage_history = compute_advantages(reward_history)
-    data.append(advantage_history)
-    writer.add_scalar('reward/average_reward', sum(reward_history) / EPS_PER_EPOCH, i)
-    writer.add_scalar('reward/average_ep_len', len(reward_history) / EPS_PER_EPOCH, i)
-    writer.add_scalar('loss/advantage', advantage_history.mean(), global_n)
-    update_policy(data)
 
 if __name__ == "__main__":
     writer = tensorboard.SummaryWriter()
     global_n = 0
 
+    # env = gym.make("CartPole-v0")
     env = gym.make("WimblepongVisualSimpleAI-v0")
     policy = Policy()
     opt = torch.optim.Adam(policy.parameters(), lr=LEARNING_RATE)
 
     model_number = -1
     if model_number != -1:
-        policy.load_state_dict(torch.load(f'results/new_{model_number}.mdl'))
+        policy.load_state_dict(torch.load(f'results/downsized_model_{model_number}.mdl'))
 
     i = 0
     while True:
-        train_iteration(i)
-        if i % 10 == 0:
+        data = collect_data()
+        reward_history = data.pop(3)
+        advantage_history = compute_advantages(reward_history)
+        data.append(advantage_history)
+        writer.add_scalar('reward/average_reward', sum(reward_history) / EPS_PER_ITERATION, i)
+        writer.add_scalar('reward/average_ep_len', len(reward_history) / EPS_PER_ITERATION, i)
+        writer.add_scalar('loss/advantage', advantage_history.mean(), global_n)
+        update_policy(data)
+
+        if i % 100 == 0:
             torch.save(policy.state_dict(), f'results/model_{i}.mdl')
         i += 1
 
