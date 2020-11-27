097248767515db54ba998a33718dc560821e9550
Hofsté Menno <hofstem1@piet.org.aalto.fi>
2020-11-27 17:47:29 +0200

    create improved training AI

--- wimblepong/__init__.py
+++ wimblepong/__init__.py
@@ -1,6 +1,7 @@
 import gym
 from wimblepong.wimblepong import Wimblepong
 from wimblepong.simple_ai import SimpleAi
+from wimblepong.improved_ai import ImprovedAi
 
 gym.envs.register("WimblepongVisualMultiplayer-v0",
         entry_point="wimblepong:Wimblepong",
@@ -11,6 +12,11 @@ gym.envs.register("WimblepongVisualSimpleAI-v0",
         entry_point="wimblepong:Wimblepong",
         max_episode_steps=None,
         kwargs={"opponent": SimpleAi, "visual": True})
+
+gym.envs.register("WimblepongVisualImprovedAI-v0",
+        entry_point="wimblepong:Wimblepong",
+        max_episode_steps=None,
+        kwargs={"opponent": ImprovedAi, "visual": True})
 
 gym.envs.register("WimblepongMultiplayer-v0",
         entry_point="wimblepong:Wimblepong",
